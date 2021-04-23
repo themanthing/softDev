f5bbf704e85e599939aef231d8af6c9917f04332
asilaev <asilaev@gmail.com>
2021-04-23 15:48:46 +0300

    new

--- homework1/import_monster/monster.py
+++ homework1/import_monster/monster.py
@@ -1,7 +1,30 @@
+import importlib
 from types import ModuleType
-from typing import List, Union, Callable
+from typing import List, Union, Callable, Optional
 
 
 def methods_importer(method_name: str, modules: List[Union[str, ModuleType]]) -> List[Callable]:
-    print(method_name)
-    print(modules)
+    such_objects = []
+    for module in modules:
+        try:
+
+            if isinstance(module, ModuleType):
+                mod = module
+                if isinstance(mod.method_name, Callable):
+                    such_objects.append(mod.method_name)
+            elif isinstance(module, str):
+                mod = importlib.import_module(module)
+                if isinstance(mod.method_name, Callable):
+                    such_objects.append(mod.method_name)
+            else:
+                raise TypeError('Must be list of strings or ModuleType')
+
+            met = getattr(mod, method, None)
+
+            if met:
+                return met
+
+        except ImportError:
+            continue
+
+    return such_objects
--- homework1/main.py
+++ homework1/main.py
@@ -4,5 +4,5 @@ import import_monster.mod
 from import_monster.mod import s
 from import_monster.monster import methods_importer
 # print(s)
-print(dir())
-methods_importer(s, import_monster.mod)
+# print(dir())
+print(methods_importer(s, import_monster.mod))
