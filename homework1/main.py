73f1766d5ac5398d13fc0ae0568d95c7c883d91a
themanthing <asilaev@gmail.com>
2021-05-03 22:55:43 +0300

    Delete homework1 directory

--- homework1/main.py
+++
@@ -1,9 +0,0 @@
-from homework1 import a
-from homework1 import b
-from import_monster.monster import methods_importer
-
-# print(dir())
-print(methods_importer("s", [a, b]))
-print(methods_importer("a", [a]))
-print(methods_importer("foo", [a, b]))
-print(methods_importer("Foo", [a]))
